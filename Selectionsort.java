e7d29d74b1cd4d469623a19c4558b7563f2f5096
Arpit Gothi <40861465+Arpitgothi23@users.noreply.github.com>
2018-07-05 22:42:07 +0530

    Add files via upload

---
+++ Selectionsort.java
@@ -0,0 +1,30 @@
+class Selectionsort
+{
+	public static void main(String[] args)
+	{
+		int a[]={23,45,12,56,78};
+		int min;
+		for(int i=0;i<(a.length);i++)
+		{
+			min=i;
+			for(int j=i+1;j<a.length;j++)
+			{
+				if(a[min]>a[j])
+				{
+					int temp=a[j];
+					a[j]=a[min];
+					a[min]=a[j];
+				}
+			}
+		}
+		
+		System.out.println("Sorted Array");
+	
+		for(int i=0;i<a.length;i++)
+			System.out.println(a[i]);
+	}
+}
+
+		
+			
+		
